bbbe0255ee94ada70ef9f6012d36753156c126b3
Alex R <alexei.rudenko@gmail.com>
2019-07-09 10:43:15 +0200

    v2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const React = require('react');
 
+const WIDGET_URL = 'https://just-comments.com/w2.js';
+
 class JustComments extends React.Component {
   constructor(...args) {
     super(...args);
@@ -23,9 +25,28 @@ class JustComments extends React.Component {
     return props;
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    const keys = Object.keys(nextProps);
+
+    return keys.some((k) => nextProps[k] !== this.props[k]);
+  }
+
   componentDidMount() {
     const s = document.createElement('script');
-    s.src = 'https://just-comments.com/w.js';
+    s.src = WIDGET_URL;
+    // s.setAttribute('data-timestamp', +new Date());
+    this.ref.current.appendChild(s);
+  }
+
+  componentDidUpdate() {
+    const s = document.createElement('script');
+    s.src = WIDGET_URL;
+    // s.setAttribute('data-timestamp', +new Date());
+    this.ref.current.innerHTML = '';
+    delete this.ref.current.dataset.rendered;
+    window.location.hash = '';
+    const style = document.getElementById('jcStyle');
+    if (style) style.parentNode.removeChild(style);
     this.ref.current.appendChild(s);
   }
 }
